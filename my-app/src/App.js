92ee5c6392b5895b42fb3b0af5ffa488a16856ff
Tomas <g00406870@atu.ie>
2024-09-30 13:56:08 +0000

    Added navigation bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,20 @@
-import Header from './Components/Header.js';
-import Content from './Components/Content.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './Components/NavigationBar';
+import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Content from './Components/Content';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Content />
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<h1>Read Component</h1>} />
+        <Route path="/create" element={<h1>Create Component</h1>} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
