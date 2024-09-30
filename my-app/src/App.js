ea7cf9f0683aeba239fc38cef3ca4b31296b5833
Tomas <g00406870@atu.ie>
2024-09-30 13:45:40 +0000

    Added Header, Content and Footer

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import Header from './Components/Header.js';
+import Content from './Components/Content.js';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
