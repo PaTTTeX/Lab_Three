1aed9667aedbf22ca6d9ca34216e8c00a5ec6e0e
PaTTeX <g00406870@atu.ie>
2024-10-07 01:44:16 +0100

    Comments for NavigationBar.js

--- my-app/src/Components/NavigationBar.js
+++ my-app/src/Components/NavigationBar.js
@@ -1,14 +1,21 @@
-import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap'; // Import Bootstrap Navbar components
+import { Link } from 'react-router-dom'; // Import Link to enable routing
 
 const NavigationBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#">React App</Navbar.Brand>
+    <Navbar bg="light" expand="lg"> {/* Create a lightthemed,  Navbar */}
+      <Navbar.Brand href="#">React App</Navbar.Brand> {/* The brand name/logo  app */}
+
+      {/* Nav section with links to different pages */}
       <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/home">Home</Nav.Link>
-        <Nav.Link as={Link} to="/read">Read</Nav.Link>
-        <Nav.Link as={Link} to="/create">Create</Nav.Link>
+        {/* Link to the Home page */}
+        <Nav.Link as={Link} to="/home">Home</Nav.Link>  
+        
+        {/* Link to the Read page */}
+        <Nav.Link as={Link} to="/read">Read</Nav.Link>  
+        
+        {/* Link to the Create page */}
+        <Nav.Link as={Link} to="/create">Create</Nav.Link>  
       </Nav>
     </Navbar>
   );
