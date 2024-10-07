d3a5b9fd8e196444a62a1857bf856e77de6e023c
PaTTeX <g00406870@atu.ie>
2024-10-07 01:41:48 +0100

    Added client side routing with comments in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,45 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import NavigationBar from './Components/NavigationBar';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Content from './Components/Content';
 
 function App() {
+  const location = useLocation(); // Gets the current URL (e.g., /create, /read)
+
   return (
-    <Router>
+    <>
+      {/* Always show the Navigation Bar */}
       <NavigationBar />
+
+      {/* If the URL is /create, show the Header */}
+      {location.pathname === '/create' && <Header />}
+
+      {/* Define the different pages (routes) */}
       <Routes>
-        <Route path="/home" element={<Content />} />
+        {/* Show the Content component when the URL is /home */}
+        <Route path="/home" element={<Content />} />  
+        
+        {/* Display "Read Component" text when the URL is /read */}
         <Route path="/read" element={<h1>Read Component</h1>} />
+        
+        {/* Display "Create Component" text when the URL is /create */}
         <Route path="/create" element={<h1>Create Component</h1>} />
       </Routes>
-      <Footer />
+
+      {/* If the URL is /read, show the Footer */}
+      {location.pathname === '/read' && <Footer />}
+    </>
+  );
+}
+
+function AppWrapper() {
+  return (
+    <Router>
+      {/* The App is wrapped in the Router to enable URL routing */}
+      <App />
     </Router>
   );
 }
 
-export default App;
+export default AppWrapper;
