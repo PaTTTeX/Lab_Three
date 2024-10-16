63908ac402d2d0a2706a35221061b09911830956
PaTTeX <g00406870@atu.ie>
2024-10-16 15:42:56 +0100

    Updated Create.js for Lab4

--- my-app/src/Components/Create.js
+++ my-app/src/Components/Create.js
@@ -1,9 +1,51 @@
-const Create = ()=>{
-    return(
-        <div>
-            <h1>This is my create component</h1>
+import { useState } from "react";
+
+function Create() {
+  const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [poster, setPoster] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      title,
+      year,
+      poster
+    });
+  }
+
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
         </div>
-    )
+        <div className="form-group">
+          <label>Movie Year: </label>
+          <input type="number"
+            className="form-control"
+            value={year}
+            onChange={(e) => { setYear(e.target.value) }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Movie Poster URL: </label>
+          <input type="text"
+            className="form-control"
+            value={poster}
+            onChange={(e) => { setPoster(e.target.value) }}
+          />
+        </div>
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
+  );
 }
 
 export default Create;
