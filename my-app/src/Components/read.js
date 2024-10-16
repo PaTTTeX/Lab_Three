b64f4457ca30237be2c0a2e48393c90bb4ddd22a
PaTTeX <g00406870@atu.ie>
2024-10-16 17:32:56 +0100

    added comments to read.js

--- my-app/src/Components/read.js
+++ my-app/src/Components/read.js
@@ -1,23 +1,30 @@
-import Movies from "./Movies";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import Movies from "./Movies"; // Importing the Movies component to display the list of movies
+import { useEffect, useState } from "react"; // Importing React hooks: useEffect and useState
+import axios from "axios"; // Importing axios for making HTTP requests
 
 function Read() {
+  // State to store the fetched movies
   const [movies, setMovies] = useState([]);
 
+  // useEffect to fetch data when the component is mounted
   useEffect(() => {
+    // Fetching the movie data from the provided API endpoint
     axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
       .then((response) => {
+        // On successful response, updating the movies state with the fetched data
         setMovies(response.data.movies);
       })
       .catch((error) => {
+        // Logging any errors in case the request fails
         console.log(error);
       });
-  }, []);
+  }, []); // Empty dependency array ensures the effect runs only once on component mount
 
   return (
     <div>
       <h2>This is my Read Component.</h2>
+      
+      {/* Passing the fetched movies to the Movies component as a prop */}
       <Movies myMovies={movies} />
     </div>
   );
