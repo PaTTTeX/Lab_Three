79064ce66d8b7394f0574e31e3363a56fa906cec
Tomas <g00406870@atu.ie>
2024-11-04 14:51:00 +0000

    Added CORS

--- my-app/BackEnd/server.js
+++ my-app/BackEnd/server.js
@@ -36,3 +36,13 @@ app.get('/api/movies', (req, res) => {
     ];
     res.json({ movies });
 });
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
